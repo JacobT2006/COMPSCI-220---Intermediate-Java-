609ee7c584c1839af74c14483f6dd473f3db301b
Jacob <115171151+JacobT2006@users.noreply.github.com>
2024-10-03 12:42:07 -0500

    In class account work

--- Week5/Account.java
+++ Week5/Account.java
@@ -1,17 +1,37 @@
 // Version-1: Account.java
 // Account class that contains an name instance variable 
 // and methods to set and get its value.
+import java.text.DecimalFormat;
 
 public class Account {
-   private String name; // instance variable
+   private String name = ""; // instance variable
    //add instance variable (or property or attribute): balance
-   
+   private double balance;
    
    //add a constructor, to provide customized initialization for a new account name and balance
+   public Account(String name, double balance){
+      this.name = name;
+      this.balance = balance;
+
+   }
+
+   public Account(){
+      //this.name = "N/A";
+      //this.name = balance;
+      this("N/A", 0);
+   }
    
+   public void deposit(double amount){
+      if(amount > 0){
+         this.balance += amount;
+      }
+   }
    
+   public double getBalance(){
+      return this.balance;
+   }
    
-   //add method: deposit() //i.e., set balance, instead of giving setBalance as the method name, "deposit" is a more proper name 
+   //add method: deposit() //i.e., set balance, instead of giving setBalance as the method name, "deposit" is a more proper name
    
    //add method: getBalance() //i.e., get balance
    
@@ -28,9 +48,10 @@ public class Account {
    }   
    
    // add method: toString()  //This is a common practice to convert account information into a string, which makes easy to print out account info.
-   public String toString() {
-	  
-	   return "";
+   public String toString(Object decimalformat) {
+      DecimalFormat moneyFormat = new DecimalFormat("$###,##0.00");
+      String accountInfo = String.format("%-10s%s", name, moneyFormat.format(balance));
+	   return "accountInfo";
    }
    
 }
--- Week5/AccountTest.java
+++ Week5/AccountTest.java
@@ -1,5 +1,5 @@
-//import java.util.ArrayList;
-//import java.util.Random;
+import java.util.ArrayList;
+import java.util.Random;
 
 //This class is used to create object instance and test the program Account.java
 
@@ -8,34 +8,60 @@ public class AccountTest{
 		
 //when in the case that no constructor defined in a class, compiler will create a default constructor, 
 //that default constructor will be called when creating object instances from that class.
-		Account aa = new Account();
-		System.out.println("Account name: " + (aa.getName().isEmpty()? "N/A": aa.getName()) );
+		Account aa = new Account("aa", 300);  // CREATING NEW OBJECT
+		System.out.println("Account name: " + (aa.getName().isEmpty()? "N/A": aa.getName()) );  //
 		
 //when in the case that at least one constructor defined in a class, then compiler will not create a default constructor,
 //Those programmer-defined constructor(s) will be called when creating object instances from that class
 		
 		//(1) add code here to create an account object named "myAccount", 
 		//and provide an account name (e.g., ask user to enter a name) and account balance
-				
+		Account myaAccount = new Account("Tina", 500);
 		//(2) display myAccount's name and balance 
-				
+		System.out.println(myaAccount.getBalance());
+		System.out.println(myaAccount.getName());
 				
 		//(3) deposit $500 to myAccount.
+		myaAccount.deposit(500);
 				
 				
 		//(4) create an array of three checking accounts
-				
+		Account[] checkingAccounts = new Account[3];
+		/*for (int i = 0; i < checkingAccounts.length; i++){
+			checkingAccounts[i] = new Account();
+			System.out.print(checkingAccounts[i]);
+		}*/
+
+		checkingAccounts[0] = new Account("sss", 100);
+		checkingAccounts[1] = new Account("aaa", 300);
+		checkingAccounts[2] = new Account("bbb", 200);
+
+		printAccounts(checkingAccounts, "Checking Account");
+
+		Account[] checkAccountsV2 = new Account[] {new Account(), new Account("Bob", 1000), new Account("Sam", 400)};
+		printAccounts(checkAccountsV2, "Checking Account");
 			
 		//(5) call printAccounts() method to print out all checking accounts info.
 				
 				
 		//(6) create an ArrayList of 10 saving accounts
-				
+		ArrayList<Account> savingsAccounts = new ArrayList<>();
+		for(int i=0; i<10; i++){
+			savingsAccounts.add(new Account());
+		}
 				
 		//(7) call printAccounts() method to print out all saving accounts info.
-				
+		Account[] savingListArray = new Account[savingsAccounts.size()];
+		savingListArray = savingsAccounts.toArray(savingListArray);
+		printAccounts(savingListArray, "Saving Account");
 		
 		
+	}
+	public static void printAccounts(Account[] checkAccountsV2, String type){ //or private
+		System.out.println("Print" + type + ":");
+		for(Account c: checkAccountsV2){
+			// N/D
+		}
 	}
 
 }
