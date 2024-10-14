853477e9ed9236982d287d67b8b1ff887678e558
Jacob <115171151+JacobT2006@users.noreply.github.com>
2024-10-14 15:10:54 -0500

    Add week7

--- Project/MyProject.java
+++ Project/MyProject.java
@@ -1,8 +1,15 @@
 package Project;
 
+import javax.swing.JOptionPane;
+
 public class MyProject {
     
     public static void main(String[] args){
+        login();    
     
+    }
+    
+    public static void login(){
+        JOptionPane.showInputDialog("yes");
     }
 }
