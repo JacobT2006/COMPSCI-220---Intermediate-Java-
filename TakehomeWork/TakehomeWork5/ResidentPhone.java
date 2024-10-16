424788ee1fdcc16994a839b361b4e871e124339f
Jacob <115171151+JacobT2006@users.noreply.github.com>
2024-10-16 18:27:52 -0500

    Update LicensePlatesSimulator

--- TakehomeWork/TakehomeWork5/ResidentPhone.java
+++ TakehomeWork/TakehomeWork5/ResidentPhone.java
@@ -1,6 +1,9 @@
 
 public class ResidentPhone {
 	//add private fields: last_name, first_name, phone_number
+	private static String last_name;
+	private static String first_name;
+	private static int phone_number;
 	
     
     //default constructor
@@ -9,10 +12,16 @@ public class ResidentPhone {
 	 
 	 
 	 //public method: getFirstName
+	 public static String getFirstName(String first_name){
+		return first_name;
+	 }
 	 
 	 //public method: setFirstName
 	 
-	//public method: getLastName
+	 //public method: getLastName
+	 public static String getLastName(String last_name){
+		return last_name;
+	 }
 	
 	 //public method: setLastName
 	 
--- Week7b/LicensePlatesSimulator.java
+++ Week7b/LicensePlatesSimulator.java
@@ -1,7 +1,11 @@
 
-import java.io.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
 public class LicensePlatesSimulator {
 
@@ -10,25 +14,55 @@ public class LicensePlatesSimulator {
 		//by using random generator, and then save them to a text file (.txt) named
 		//"license_plates".
 	//step-1: create a Random object as a random number generator 
-		
+		Random rGenerator = new Random();
 		
 	//step-2: define a ArrayList of Strings to store licensePlates
-	//call getRandomPlates() method to create that ArrayList	
+	//call getRandomPlates() method to create that ArrayList
+		ArrayList<String> plates = getRandomPlates(rGenerator,100);
 	
-		
 	//output to a text file
 		String filename = "license_plates.txt";
 		
 	//Step-3: Use PrintWriter and FileWriter objects to write random generated license plates to the output file	
+		File file = new File(filename);
+		PrintWriter outputFile = new PrintWriter(new FileWriter(file, false));
+		for (String p: plates){
+			outputFile.println(p);
+		}
 		
 
+		//Step-4: closes the PrintWriter and releases any system resources associated with the file output
+	    System.out.println("Data is saved to " + (new File(filename)).getAbsolutePath()	);
+		outputFile.close();
 		
 		
-	//Step-4: closes the PrintWriter and releases any system resources associated with the file output
-	    
-		
+
+
+		//part 2 get the rest 50 liscense plates
+
+
+		outputFile = new PrintWriter(new FileWriter(file, true));
+		plates = getRandomPlates(rGenerator,50);
+		for (String p: plates){
+			outputFile.println(p);
+		}
+
+		outputFile.close();
 		System.out.println("Data is saved to " + (new File(filename)).getAbsolutePath()	);
 	    
+		File dataFile = new File("license_plates.txt");
+		Scanner keyboard = new Scanner(dataFile);
+
+		System.out.println("Read from file " + dataFile.getAbsolutePath()); 
+
+		int cnt = 0;
+		while(keyboard.hasNextLine()){
+			String licenseString = keyboard.nextLine();
+			System.out.println(++cnt + " " + licenseString);
+		}
+		
+		
+	
 	}
 	/**
 	 * getRandomPlates: generate license plate numbers by using random number generator
@@ -38,10 +72,16 @@ public class LicensePlatesSimulator {
 	 */
 	public static ArrayList<String> getRandomPlates(Random rGenerator, int numR){
 		ArrayList<String> plates = new ArrayList<String>();
-		String p1 = "", p2="";
+		String 	p1 = "", 
+			   	p2 = "";
 		
 	//Step-5: add a for loop to generate part-1 and part-2 of the license plates
 		//call getRandom() method to get part-1 and part-2 string
+		for (int i=0; i<numR; i++){
+			p1 = getRandom(rGenerator, 65, 90, 3);
+			p2 = getRandom(rGenerator, 48, 57, 4);
+			plates.add(p1 + "-" + p2);
+		}
 		
 		return plates;
 	}
